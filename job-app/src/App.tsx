66d11d98734bcfb30b3d1766b1b8f274eedd5ec2
Anastasia <nastia.shrai@gmail.com>
2022-11-12 20:14:37 +0200

    init: create basic structure

---
+++ job-app/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className='p-3'>
+        <h1 className="text-3xl text-gray-800 font-bold">
+          Job App
+        </h1>
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
