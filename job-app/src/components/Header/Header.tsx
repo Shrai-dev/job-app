f22eac2c624658d95403e8cc1dd2abab510a3c74
Anastasia <nastia.shrai@gmail.com>
2022-11-16 17:55:46 +0200

    feat: add Header component

---
+++ job-app/src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+	return (
+		<header className='p-3'>
+        <h1 className="text-3xl text-gray-800 font-bold text-center">
+          Job App
+        </h1>
+      </header>
+	)
+}
