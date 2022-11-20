008ef2c8de624f96528b97772d1b17882c08396e
Anastasia <nastia.shrai@gmail.com>
2022-11-20 19:56:17 +0200

    feat: add routing

--- job-app/src/App.tsx
+++ job-app/src/App.tsx
@@ -1,14 +1,22 @@
+import { FC } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
-function App() {
+import { Header } from './components/Header/Header';
+import { JobList } from './components/JobList/JobList';
+import { JobDetails } from './components/JobDetails/JobDetails';
+
+
+const App: FC = () => {
+
   return (
-    <div className="App">
-      <header className='p-3'>
-        <h1 className="text-3xl text-gray-800 font-bold">
-          Job App
-        </h1>
-      </header>
-      
+    <div className="bg-gray-200">
+      <Header />
+      <Routes>
+        <Route path='/' element={<JobList/>} />
+        <Route path='/jobs' element={<JobList/>} />
+        <Route path='/jobs/:jobId' element={<JobDetails/>} />
+      </Routes>
     </div>
   );
 }
